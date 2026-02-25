8fdc789dd733ab37f20dfefcfd4bda95f6fcb24f
superzaky <superkytoz@gmail.com>
2026-02-25 23:20:25 +0100

    edit telescope.lua

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -7,46 +7,54 @@ return {
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
         }, 
         config = function()
-			require('telescope').setup{
-				defaults = {
-					-- Default configuration for telescope goes here:
-					-- config_key = value,
-					mappings = {
-						i = {
-							-- map actions.which_key to <C-h> (default: <C-/>)
-							-- actions.which_key shows the mappings for your picker,
-							-- e.g. git_{create, delete, ...}_branch for the git_branches picker
-							["<C-h>"] = "which_key"
-						}
-					}
-				},
+            require('telescope').setup{
+                defaults = {
+                    -- Default configuration for telescope goes here:
+                    -- config_key = value,
+                    mappings = {
+                        i = {
+                            -- map actions.which_key to <C-h> (default: <C-/>)
+                            -- actions.which_key shows the mappings for your picker,
+                            -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                            ["<C-h>"] = "which_key"
+                        }
+                    }
+                },
                 pickers = {
                     find_files = {
                         theme="ivy"
                     }
-					-- Default configuration for builtin pickers goes here:
-					-- picker_name = {
-						--   picker_config_key = value,
-						--   ...
-						-- }
-						-- Now the picker_config_key will be applied every time you call this
-						-- builtin picker
-					},
-					extensions = {
-						-- Your extension configuration goes here:
-						-- extension_name = {
-							--   extension_config_key = value,
-							-- }
-							-- please take a look at the readme of the extension you want to configure
-						}
-			}
+                    -- Default configuration for builtin pickers goes here:
+                    -- picker_name = {
+                        --   picker_config_key = value,
+                        --   ...
+                        -- }
+                        -- Now the picker_config_key will be applied every time you call this
+                        -- builtin picker
+                    },
+                    extensions = {
+                        -- Your extension configuration goes here:
+                        -- extension_name = {
+                            --   extension_config_key = value,
+                            -- }
+                            -- please take a look at the readme of the extension you want to configure
+                        }
+            }
             vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
             vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+           -- to search for files or folders inside of ~/.config/nvim 
             vim.keymap.set("n", "<space>en", function ()
                 require('telescope.builtin').find_files{
                     cwd = vim.fn.stdpath("config")
                 }
             end)
+            -- to search for files or folders of plugins
+            vim.keymap.set("n", "<space>ep", function()
+                require('telescope.builtin').find_files {
+                    cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+                }
+            end)
+            require "config.telescope.multigrep".setup()
         end
     }
 }
--- lua/zaky/remap.lua
+++ lua/zaky/remap.lua
@@ -3,3 +3,31 @@ vim.g.mapleader =  " "
 -- when opening a folder and then a file via VIM, then you can press space pv to go back to the browser page
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- onderstaande auto refresh werkt niet, dus gebruik iets anders
+-- -- Create a variable to track the state
+-- vim.g.auto_refresh_enabled = false
+
+-- -- Function to toggle the behavior
+-- function ToggleAutoRefresh()
+--   if vim.g.auto_refresh_enabled then
+--     vim.api.nvim_clear_autocmds({ group = "AutoRefresh" })
+--     vim.g.auto_refresh_enabled = false
+--     print("Auto refresh disabled")
+--   else
+--     vim.o.autoread = true
+--     vim.api.nvim_create_augroup("AutoRefresh", { clear = true })
+--     vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+--       group = "AutoRefresh",
+--       command = "if mode() != 'c' | checktime | endif",
+--       pattern = "*",
+--     })
+--     vim.g.auto_refresh_enabled = true
+--     print("Auto refresh enabled")
+--   end
+-- end
+
+-- vim.keymap.set('n', '<leader>ar', ToggleAutoRefresh, { 
+--   noremap = true, 
+--   silent = false,
+--   desc = "Toggle auto refresh of files" 
+-- })
