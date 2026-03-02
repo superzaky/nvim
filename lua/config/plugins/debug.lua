e656bfa2d2a43638d20f4d09085e7b9d6b8b3c29
superzaky <superkytoz@gmail.com>
2026-03-03 00:20:11 +0100

    debugger for .NET

--- lua/config/plugins/debug.lua
+++ lua/config/plugins/debug.lua
@@ -19,25 +19,31 @@ return {
         
         dap_python.setup(python_path)
 
-        -- 2. Debugging Keymaps
+        -- 2. Setup .NET Adapter (netcoredbg)
+        dap.adapters.coreclr = {
+            type = 'executable',
+            -- Mason installs it to a 'bin' folder usually, but check your Mason install
+            --   command = vim.fn.stdpath("data") .. '/mason/bin/netcoredbg' .. (vim.fn.has("win32") == 1 and ".exe" or ""),
+            command = 'C:/projects/netcoredbg/netcoredbg.exe',
+            args = { '--interpreter=vscode' }
+        }
+
+        -- 3. Debugging Keymaps
         vim.keymap.set('n', '<leader>dc', function() dap.continue() end, { desc = "Debug: Continue" })
-        -- for some reason F5 moves the cursor only downwards in qterminal and does not start the debugger for some reason, so that is why we comment the line below
-        -- vim.keymap.set('n', '<F5>', function() dap.continue() end, { desc = "Debug: Start/Continue" })
         vim.keymap.set('n', '<F10>', function() dap.step_over() end, { desc = "Debug: Step Over" })
         vim.keymap.set('n', '<F11>', function() dap.step_into() end, { desc = "Debug: Step Into" })
         vim.keymap.set('n', '<leader>b', function() dap.toggle_breakpoint() end, { desc = "Debug: Toggle Breakpoint" })
 
-        -- 3. UI Setup
+        -- 4. UI Setup
         local dapui = require("dapui")
         dapui.setup()
         dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
         dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
         dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
 
-        -- 4. Custom Python Configurations
+        -- 5. Custom Python Configurations
         dap.configurations.python = {
             {
-                -- Standard Launch (Works on Linux or Windows)
                 type = 'python',
                 request = 'launch',
                 name = "Launch file",
@@ -45,7 +51,7 @@ return {
                 pythonPath = python_path,
             },
             {
-                -- Corporate Attach (For your PowerShell manual launch)
+                -- Corporate Attach
                 type = 'python',
                 request = 'attach',
                 name = "Attach (Windows Port 5678)",
@@ -53,7 +59,6 @@ return {
                     host = '127.0.0.1',
                     port = 5678,
                 },
-                -- This helps Neovim find the source code on your machine
                 pathMappings = {
                     {
                         localRoot = vim.fn.getcwd(),
@@ -61,6 +66,19 @@ return {
                     },
                 },
             },
+        }
+
+        -- 6. Custom .NET Configurations
+        dap.configurations.cs = {
+          {
+            type = "coreclr",
+            name = "Launch - netcoredbg",
+            request = "launch",
+            program = function()
+                -- Asks you to point to the compiled .dll file
+                return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+            end,
+          },
         }
     end,
 }
